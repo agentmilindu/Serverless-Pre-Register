834a977ace32b9445f2804369955cbb7ff7fcaff
agentmilindu <agentmilindu@gmail.com>
2016-01-04 11:20:41 +0000

    checking if the repo is clonned before clonning

--- back/modules/git/webhook/handler.js
+++ back/modules/git/webhook/handler.js
@@ -19,36 +19,51 @@ var dynamodb = new AWS.DynamoDB({
 });
 
 var Git = require("nodegit");
+var fs = require('fs');
 
 
 // Lambda Handler
 module.exports.handler = function(event, context) {
   var tableName = "Git";
   var datetime = new Date().getTime().toString();
+  var repoURL = "https://github.com/agentmilindu/Serverless-Pre-Register.git";
+  var repoDir = "/tmp/repo";
 
-  Git.Clone("https://github.com/agentmilindu/Serverless-Pre-Register.git", "/tmp/repo").then(function(repository) {
-    dynamodb.putItem({
-      "TableName": tableName,
-      "Item": {
-        "id": {
-          "S": Date.now().toString()
-        },
-        "json": {
-          "S": JSON.stringify(repository)
-        }
-      }
-    }, function(err, data) {
-      if (err) {
-        context.done('error', 'putting item into dynamodb failed: ' + err + " " + event.name);
-        console.log('error: ' + JSON.stringify(err, null, '  '));
-      }
-      else {
-        console.log('great success: ' + JSON.stringify(data, null, '  '));
-        context.done('OK');
-      }
-    });
-  }, 
-  function(e){
-	console.log(e);
+  fs.access(repoDir, fs.R_OK | fs.W_OK, function(err) {
+    ((!err) ? Git.Repository.open(repoDir) :
+      Git.Clone(repoURL, repoDir, null))
+    .then(function(repository) {
+        return repository.getBranchCommit("master");
+      })
+      .then(function(commit) {
+        return commit.getEntry("README.md");
+      })
+      .then(function(entry) {
+        console.log(entry);
+        dynamodb.putItem({
+          "TableName": tableName,
+          "Item": {
+            "id": {
+              "S": Date.now().toString()
+            },
+            "json": {
+              "S": JSON.stringify(entry)
+            }
+          }
+        }, function(err, data) {
+          if (err) {
+            context.done('error', 'putting item into dynamodb failed: ' + err + " " + event.name);
+            console.log('error: ' + JSON.stringify(err, null, '  '));
+          }
+          else {
+            console.log('great success: ' + JSON.stringify(data, null, '  '));
+            context.done('OK');
+          }
+        });
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
   });
+
 };

2d593bfc24fa2035997fa88aedf64bbe306d0496
Milindu Sanoj Kumarage <milinduk@99x.lk>
2016-01-21 10:57:44 +0530

    optimized

--- back/modules/user/lib/index.js
+++ back/modules/user/lib/index.js
@@ -1,23 +1,14 @@
-/**
- * Lib
- */
-
 var AWS = require('aws-sdk');
 var dynamodb = new AWS.DynamoDB({
   apiVersion: '2012-08-10'
 });
 
 var tableName = "Users";
-
 var password = require('password-hash-and-salt');
-
 
 module.exports.login = function(event, cb) {
 
   var email = event.email;
-
-  var response = {}
-
   var params = {
     AttributesToGet: [
       "password"
@@ -29,47 +20,36 @@ module.exports.login = function(event, cb) {
       }
     }
   }
-
-  var myuser = [];
-
   dynamodb.getItem(params, function(err, passwordHash) {
     if (err) {
-      console.log(err); // an error occurred
-      response = {
+      return cb(null,  {
         "message": "We had errors getting user details",
         "error": JSON.stringify(err)
-      }
-      return cb(null, response);
+      });
     }
     else {
-      // Verifying the hash
       password(event.password).verifyAgainst(passwordHash, function(error, verified) {
         if (error)
           throw new Error('Something went wrong!');
         if (!verified) {
-          var response = {
+          return cb(null, = {
             message: "Your username or password did not match!",
             code : 403
-          };
-          return cb(null, response);
+          });
         }
         else {
-          var response = {
+          return cb(null, {
             message: "Logged in successfully!",
             code : 200
-          };
-          return cb(null, response);
+          });
         }
       });
     }
   });
-
 };
 
 module.exports.signup = function(event, cb) {
-
   var datetime = new Date().getTime().toString();
-
   password(event.password).hash(function(error, hash) {
     if(error)
     throw new Error('Something went wrong!');
@@ -100,8 +80,6 @@ module.exports.signup = function(event, cb) {
     });
 
   });
-
-
 };
 
 module.exports.prereg = function(event, context) {
@@ -128,5 +106,4 @@ module.exports.prereg = function(event, context) {
       return cb(null, { message: "great success" });
     }
   });
-
 };

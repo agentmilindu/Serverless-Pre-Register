5c514b455bcbb37b1202766c7c235640e10f4f7d
Milindu Sanoj Kumarage <milinduk@99x.lk>
2016-01-21 10:29:56 +0530

    Password hashing added to user signup

--- back/modules/user/lib/index.js
+++ back/modules/user/lib/index.js
@@ -12,15 +12,6 @@ var tableName = "Users";
 var password = require('password-hash-and-salt');
 
 
-module.exports.respond = function(event, cb) {
-
-  var response = {
-    message: "Your Serverless function ran successfully!"
-  };
-
-  return cb(null, response);
-};
-
 module.exports.login = function(event, cb) {
 
   var email = event.email;
@@ -51,7 +42,7 @@ module.exports.login = function(event, cb) {
       return cb(null, response);
     }
     else {
-      // Verifying the hash 
+      // Verifying the hash
       password(event.password).verifyAgainst(passwordHash, function(error, verified) {
         if (error)
           throw new Error('Something went wrong!');
@@ -70,6 +61,75 @@ module.exports.login = function(event, cb) {
           return cb(null, response);
         }
       });
+    }
+  });
+
+};
+
+module.exports.signup = function(event, cb) {
+
+  var datetime = new Date().getTime().toString();
+
+  password(event.password).hash(function(error, hash) {
+    if(error)
+    throw new Error('Something went wrong!');
+
+    dynamodb.putItem({
+      "TableName": tableName,
+      "Item": {
+        "name": {
+          "S": event.name
+        },
+        "email": {
+          "S": event.email
+        },
+        "password": {
+          "S": hash
+        },
+        "date": {
+          "S": datetime
+        }
+      }
+    }, function(err, data) {
+      if (err) {
+        return cb(null, { message: "You got registered successfully!" });
+      }
+      else {
+        return cb(null,  { message: "We could not register you successfully!" });
+      }
+    });
+
+  });
+
+
+};
+
+module.exports.preReg = function(event, context) {
+  var tableName = "pre-regs";
+  var datetime = new Date().getTime().toString();
+  dynamodb.putItem({
+    "TableName": tableName,
+    "Item": {
+      "name": {
+        "S": event.name
+      },
+      "email": {
+        "S": event.email
+      },
+      "previewAccess": {
+        "S": event.previewAccess
+      },
+      "date": {
+        "S": datetime
+      }
+    }
+  }, function(err, data) {
+    if (err) {
+      context.done('putting item into dynamodb failed: ' + err);
+    }
+    else {
+      console.log('great success: ' + JSON.stringify(data, null, '  '));
+      context.succeed('OK');
     }
   });
 

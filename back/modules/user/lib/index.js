1ae06500e9c97320bde75d396688d2831a1c37c0
Milindu Sanoj Kumarage <milinduk@99x.lk>
2016-01-21 12:32:07 +0530

    Revert "optimized"
    This reverts commit 2d593bfc24fa2035997fa88aedf64bbe306d0496.

--- back/modules/user/lib/index.js
+++ back/modules/user/lib/index.js
@@ -1,14 +1,23 @@
+/**
+ * Lib
+ */
+
 var AWS = require('aws-sdk');
 var dynamodb = new AWS.DynamoDB({
   apiVersion: '2012-08-10'
 });
 
 var tableName = "Users";
+
 var password = require('password-hash-and-salt');
+
 
 module.exports.login = function(event, cb) {
 
   var email = event.email;
+
+  var response = {}
+
   var params = {
     AttributesToGet: [
       "password"
@@ -20,36 +29,47 @@ module.exports.login = function(event, cb) {
       }
     }
   }
+
+  var myuser = [];
+
   dynamodb.getItem(params, function(err, passwordHash) {
     if (err) {
-      return cb(null,  {
+      console.log(err); // an error occurred
+      response = {
         "message": "We had errors getting user details",
         "error": JSON.stringify(err)
-      });
+      }
+      return cb(null, response);
     }
     else {
+      // Verifying the hash
       password(event.password).verifyAgainst(passwordHash, function(error, verified) {
         if (error)
           throw new Error('Something went wrong!');
         if (!verified) {
-          return cb(null, = {
+          var response = {
             message: "Your username or password did not match!",
             code : 403
-          });
+          };
+          return cb(null, response);
         }
         else {
-          return cb(null, {
+          var response = {
             message: "Logged in successfully!",
             code : 200
-          });
+          };
+          return cb(null, response);
         }
       });
     }
   });
+
 };
 
 module.exports.signup = function(event, cb) {
+
   var datetime = new Date().getTime().toString();
+
   password(event.password).hash(function(error, hash) {
     if(error)
     throw new Error('Something went wrong!');
@@ -80,6 +100,8 @@ module.exports.signup = function(event, cb) {
     });
 
   });
+
+
 };
 
 module.exports.prereg = function(event, context) {
@@ -106,4 +128,5 @@ module.exports.prereg = function(event, context) {
       return cb(null, { message: "great success" });
     }
   });
+
 };

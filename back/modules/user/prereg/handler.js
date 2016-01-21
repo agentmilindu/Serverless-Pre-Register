7d737782103c32d46de5c157dd4d61a0f5721f1e
Milindu Sanoj Kumarage <milinduk@99x.lk>
2016-01-21 14:16:37 +0530

    moved prereg function to lib

--- back/modules/user/prereg/handler.js
+++ back/modules/user/prereg/handler.js
@@ -10,6 +10,9 @@
 // Require Serverless ENV vars
 var ServerlessHelpers = require('serverless-helpers-js').loadEnv();
 
+// Require Logic
+var user = require('../lib');
+
 var AWS = require('aws-sdk');
 var dynamodb = new AWS.DynamoDB({
     apiVersion: '2012-08-10'
@@ -18,33 +21,7 @@ var dynamodb = new AWS.DynamoDB({
 
 // Lambda Handler
 module.exports.handler = function(event, context) {
-  prereg(event, function(error, response) {
+  user.prereg(event, function(error, response) {
     return context.done(error, response);
-  });
-};
-
-function prereg = function(event, context) {
-  var tableName = "pre-regs";
-  var datetime = new Date().getTime().toString();
-  dynamodb.putItem({
-    "TableName": tableName,
-    "Item": {
-      "name": {
-        "S": event.name
-      },
-      "email": {
-        "S": event.email
-      },
-      "date": {
-        "S": datetime
-      }
-    }
-  }, function(err, data) {
-    if (err) {
-      return cb(null, { message: "putting item into dynamodb failed" });
-    }
-    else {
-      return cb(null, { message: "great success" });
-    }
   });
 };
